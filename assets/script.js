1a94286a6352fa06c11603bc59386f39b9fb0bac
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2026-04-30 13:24:24 +0200

    Add eventListener once in each button element

--- assets/script.js
+++ assets/script.js
@@ -26,14 +26,6 @@ buttonAddBook.addEventListener("click", (event)=> {
 
     addBookToLibrary(author, title, pages, read);
     ShowBooksOnScreen();
-
-    buttonRemove = document.querySelectorAll(".remove");
-    buttonRemove = Array.from(buttonRemove);
-    buttonRemove.forEach(deleteArrayItem);
-
-    buttonToggle = document.querySelectorAll(".toggle");
-    buttonToggle = Array.from(buttonToggle);
-    buttonToggle.forEach(ToggleRead);
     
     myForm.reset();
 })
@@ -82,57 +74,67 @@ function ShowBooksOnScreen() {
 
     if(myLibrary.length > 2) {
 
-        console.log(1)
-
-        let tableRow = document.createElement("tr");
-        tbody.appendChild(tableRow);
+        let card = document.createElement("div");
+        card.classList.add("card")
+        cards.appendChild(card);
 
         let arrayLastIndex = myLibrary.length-1;
 
-        let tdID = document.createElement("td");
-        let tdAuthor = document.createElement("td");
-        let tdTitle = document.createElement("td");
-        let tdPages = document.createElement("td");
-        let tdRead = document.createElement("td");
-        let tdDelete = document.createElement("td");
-        let tdToggle = document.createElement("td");
+        let cardHeader = document.createElement("div");
+        let cardBody = document.createElement("div");
+        let cardFooter = document.createElement("div");
+
+        cardHeader.classList.add("card-header");
+        cardBody.classList.add("card-body");
+        cardFooter.classList.add("card-footer");
+
+        let title = document.createElement("h1");
+        let author = document.createElement("h2");
+
+        let pages = document.createElement("p");
+        let read = document.createElement("p");
+
         let buttonRemove = document.createElement("button");
         let buttonToggle = document.createElement("button");
 
 
-        tdID.setAttribute("data-id", `${myLibrary[arrayLastIndex]["id"]}`);
         buttonRemove.setAttribute("data-id", `${myLibrary[arrayLastIndex]["id"]}`);
         buttonRemove.setAttribute("class", "remove");
         buttonToggle.setAttribute("data-id", `${myLibrary[arrayLastIndex]["id"]}`);
-        tdRead.setAttribute("data-id", `${myLibrary[arrayLastIndex]["id"]}`);
-        tdRead.setAttribute("class", "read");
+        read.setAttribute("data-id", `${myLibrary[arrayLastIndex]["id"]}`);
+        read.setAttribute("class", "read");
         buttonToggle.setAttribute("class", "toggle");
 
-        let read; 
+        let readStaus; 
         if (myLibrary[arrayLastIndex]["read"] == true) {
-            read = "Yes";
+            readStaus = "Yes";
         } else {
-            read = "No"
+            readStaus = "No"
         }
 
-        tdID.textContent = myLibrary[arrayLastIndex]["id"];
-        tdAuthor.textContent = myLibrary[arrayLastIndex]["author"];
-        tdTitle.textContent = myLibrary[arrayLastIndex]["title"];
-        tdPages.textContent = myLibrary[arrayLastIndex]["pages"];
-        tdRead.textContent = read;
+        author.textContent = myLibrary[arrayLastIndex]["author"];
+        title.textContent = myLibrary[arrayLastIndex]["title"];
+        pages.textContent = `Pages ${myLibrary[arrayLastIndex]["pages"]}`;
+        read.textContent = `Read: ${readStaus}`;
         buttonRemove.textContent = "Remove";
         buttonToggle.textContent = "Toggle Read";
 
+        buttonToggle.addEventListener("click", ToggleRead);
+        buttonRemove.addEventListener("click", deleteArrayItem);
 
-        tableRow.appendChild(tdID);
-        tableRow.appendChild(tdAuthor);
-        tableRow.appendChild(tdTitle);
-        tableRow.appendChild(tdPages);
-        tableRow.appendChild(tdRead);
-        tableRow.appendChild(tdDelete);
-        tableRow.appendChild(tdToggle);
-        tdDelete.appendChild(buttonRemove);
-        tdToggle.appendChild(buttonToggle);
+        cards.appendChild(card);
+
+        card.appendChild(cardHeader);
+        cardHeader.appendChild(title);
+        cardHeader.appendChild(author);
+
+        card.appendChild(cardBody);
+        cardBody.appendChild(pages);
+        cardBody.appendChild(read);
+
+        card.appendChild(cardFooter);
+        cardFooter.appendChild(buttonRemove);
+        cardFooter.appendChild(buttonToggle);
 
     } else {
 
@@ -184,6 +186,9 @@ function ShowBooksOnScreen() {
             buttonRemove.textContent = "Remove";
             buttonToggle.textContent = "Toggle Read";
 
+            buttonToggle.addEventListener("click", ToggleRead);
+            buttonRemove.addEventListener("click", deleteArrayItem);
+
             cards.appendChild(card);
 
             card.appendChild(cardHeader);
@@ -206,53 +211,40 @@ addBookToLibrary("Coolen Hover", "Verity", 320, true);
 addBookToLibrary("Brian P. Moran e Michael Lennington", "O ano de 12 semanas", 208, false);
 ShowBooksOnScreen();
 
-let buttonRemove = document.querySelectorAll(".remove");
+function ToggleRead(event) {
 
-let buttonToggle = document.querySelectorAll(".toggle");
+    let bookUuid = event.target.dataset.id;
+    let read = document.querySelector(`p[data-id="${bookUuid}"].read`);
 
-buttonRemove = Array.from(buttonRemove);
-buttonToggle = Array.from(buttonToggle);
-
-buttonRemove.forEach(deleteArrayItem);
-buttonToggle.forEach(ToggleRead);
-
-function ToggleRead(element) {
-    element.addEventListener("click", ()=> {
-        let bookUuid = element.dataset.id;
-        let read = document.querySelector(`p[data-id="${bookUuid}"].read`);
-
-        let bookIndex = myLibrary.findIndex((book)=>{
-            return book.id == bookUuid;
-        })
-
-        myLibrary[bookIndex].toggleReadStatus();
-
-        let readStaus; 
-        if (myLibrary[bookIndex]["read"] == true) {
-            readStaus = "Yes";
-        } else {
-            readStaus = "No";
-        }
-
-        read.textContent = `Read: ${readStaus}`;
-
+    let bookIndex = myLibrary.findIndex((book)=>{
+        return book.id == bookUuid;
     })
+
+    myLibrary[bookIndex].toggleReadStatus();
+
+    let readStaus; 
+    if (myLibrary[bookIndex]["read"] == true) {
+        readStaus = "Yes";
+    } else {
+        readStaus = "No";
+    }
+
+    read.textContent = `Read: ${readStaus}`;
+
 }
 
-function deleteArrayItem(element) {
-    element.addEventListener("click", ()=> {
+function deleteArrayItem(event) {
 
-        let bookUuid = element.dataset.id;
-        let cardFooter = element.parentNode;
-        let card = cardFooter.parentNode;
-        
-        card.remove()
+    let bookUuid = event.target.dataset.id;
+    let cardFooter = event.target.parentNode;
+    let card = cardFooter.parentNode;
+    
+    card.remove()
 
-        let bookIndex = myLibrary.findIndex((book)=>{
-            return book.id == bookUuid;
-        })
+    let bookIndex = myLibrary.findIndex((book)=>{
+        return book.id == bookUuid;
+    })
 
-        myLibrary.splice(bookIndex, 1);
+    myLibrary.splice(bookIndex, 1);
 
-    });
 }
